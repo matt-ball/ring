10fb09954d930cb51e05aa4005f93c7841cc8d0f
mjoliver <matthew.j.oliver1@gmail.com>
2017-06-10 02:29:05 +0100

    Some JS stuff before bed

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,16 @@ const $ = window.jQuery
 
 $.get('/api/status').then(append)
 
-function append (data) {
+function append (data, ringinfo) {
   $('body').prepend(JSON.stringify(data))
-  $('.track-status').text(data.trackOpen.nords ? 'Open' : 'Closed')
+  $('.track-status').append('Nords:' + (data.trackOpen.nords ? 'Open' : 'Closed'))
+  $('.gp-status').append(' GP:' + (data.trackOpen.gp ? 'Open' : 'Closed'))
+  $('.carsOnTrack').append('Cars:' + data.carsOnTrack)
+  $('.bikesOnTrack').append(' Bikes:' + data.bikesOnTrack)
+  $('.vehiclesOnTrack').append(' Vehicles:' + data.vehiclesOnTrack)
+  if (data.nordschleifeIsOpenedToday = true) {
+  	$('.open-hours').append(data.nordschleifeDescription)
+  } else {
+  		$('.open-hours').text('Track is not open today.')
+  }
 }
