97fa94599f95a05ec7a52a3cddb9d7ce1f1f8c10
mjoliver <matthew.j.oliver1@gmail.com>
2017-06-10 15:12:44 +0100

    cleanup of if logic from ball's comment

--- public/js/client.js
+++ public/js/client.js
@@ -9,7 +9,7 @@ function append (data, ringinfo) {
   $('.carsOnTrack').append('Cars:' + data.carsOnTrack)
   $('.bikesOnTrack').append(' Bikes:' + data.bikesOnTrack)
   $('.vehiclesOnTrack').append(' Vehicles:' + data.vehiclesOnTrack)
-  if (data.nordschleifeIsOpenedToday = true) {
+  if (data.nordschleifeIsOpenedToday) {
   	$('.open-hours').append(data.nordschleifeDescription)
   } else {
   	$('.open-hours').text('Track is not open today.')
