6597bf17d2f26fda44ec8aa94a0fb3c18115aee8
Matt Ball <matt.ball@qubit.com>
2017-10-18 22:10:33 +0100

    tidy up

--- public/js/client.js
+++ public/js/client.js
@@ -7,21 +7,16 @@ function append (data) {
 
   const nordsOpen = data.trackOpen.nords ? 'Open' : 'Closed'
   const gpOpen = data.trackOpen.gp ? 'Open' : 'Closed'
-  $('.track-status').text(`Nordschleife: ${nordsOpen} | GP: ${gpOpen}`)
-
-  $('.vehiclesOnTrack').text(`Cars: ${data.carsOnTrack} | Bikes: ${data.bikesOnTrack}`)
-
-  const openingHours = data.nordschleifeDescription.split(' Uhr')[0] || 'Closed'
-  $('.open-hours').text(`Nordschleife: ${openingHours}`)
-
-  const trackInfo = data.trackInfo.text
-  $('.track-info').text(`Track Info: ${trackInfo}`)
-  if ($('.track-info').val().indexOf('null')) {
-  	$('.track-info').text('Track is Clear')
-  }
-
   const sectionInfo = data.trackInfo.sections
-  if (sectionInfo.indexof() > -1) {
-  	  $('.section-info').text(`Sections: ${sectionInfo}`)
+  const trackInfo = data.trackInfo.text
+  let openingHours = data.nordschleifeDescription.split(' Uhr')[0]
+
+  if (openingHours === 'geschlossen') {
+    openingHours = 'Closed'
   }
+
+  $('.track-status').text(`Nordschleife: ${nordsOpen} | GP: ${gpOpen}`)
+  $('.vehiclesOnTrack').text(`Cars: ${data.carsOnTrack} | Bikes: ${data.bikesOnTrack}`)
+  $('.open-hours').text(`Nordschleife: ${openingHours}`)
+  $('.track-info').text(`Track Info: ${trackInfo}`)
 }
