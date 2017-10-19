c26df5a22e19a3122de10ba2e419cc531bfddb70
Matt Ball <matt.ball@qubit.com>
2017-10-19 22:59:47 +0100

    new ui

--- public/js/client.js
+++ public/js/client.js
@@ -3,26 +3,39 @@ const $ = window.jQuery
 $.get('/api/status').then(append)
 
 function append (data) {
-  $('body').prepend(JSON.stringify(data))
+  console.log(data)
 
   const nordsOpen = data.trackOpen.nords ? 'Open' : 'Closed'
   const gpOpen = data.trackOpen.gp ? 'Open' : 'Closed'
   const sectionInfo = data.trackInfo.sections
-  const trackInfo = data.trackInfo.text || 'Closed'
+  const trackInfo = data.trackInfo.text || 'Track clear'
   const openingHours = data.times
-  let gpOpenTimes = 'Closed'
-  let nordsOpenTimes = 'Closed'
+  let gpOpenTimes = 'Shut today'
+  let nordsOpenTimes = 'Shut today'
 
   if (data.trackOpen.gp) {
     gpOpenTimes = `Open: ${openingHours.gp.open} Close: ${openingHours.gp.close}`
+    $('.track-status-gp').removeClass('track-status-closed')
+    $('.track-status-gp').addClass('track-status-open')
+  } else {
+
+    $('.track-status-gp').removeClass('track-status-open')
+    $('.track-status-gp').addClass('track-status-closed')
   }
 
   if (data.trackOpen.nords) {
     nordsOpenTimes = `Open: ${openingHours.nords.open} Close: ${openingHours.nords.close}`
+    $('.track-status-nords').removeClass('track-status-closed')
+    $('.track-status-nords').addClass('track-status-open')
+  } else {
+    $('.track-status-nords').removeClass('track-status-open')
+    $('.track-status-nords').addClass('track-status-closed')
   }
 
-  $('.track-status').text(`Nordschleife: ${nordsOpen} | GP: ${gpOpen}`)
-  $('.vehiclesOnTrack').text(`Cars: ${data.carsOnTrack} | Bikes: ${data.bikesOnTrack}`)
-  $('.open-hours').text(`Nordschleife: ${nordsOpenTimes} | GP: ${gpOpenTimes}`)
+  $('.track-status-nords span').text(nordsOpen)
+  $('.track-status-gp span').text(gpOpen)
+  $('.cars').text(data.carsOnTrack)
+  $('.bikes').text(data.bikesOnTrack)
+  $('.open-hours').html(`<strong>Nordschleife:</strong> ${nordsOpenTimes} | <strong>GP:</strong> ${gpOpenTimes}`)
   $('.track-info').text(`${trackInfo}`)
 }
